e816d7d9f9f90032c1c6d2e226143d539f37af56
amoqOffice <amoq.office@gmail.com>
2023-03-31 07:01:31 +0200

    texte ajouté

--- show.php
+++ show.php
@@ -1,3 +1,30 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Meta tags requis pour Bootstrap -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Lien vers la feuille de style Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <title>Exemple Bootstrap</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-3">
+          <p class="text-left" style="font-size: 50px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor finibus magna, eget laoreet enim pulvinar vel. Proin commodo commodo nulla, ac ullamcorper orci ultricies eu. Praesent faucibus malesuada dolor, a consectetur nulla viverra ac. Sed vel semper urna, quis efficitur nisl. Donec id lobortis neque. Sed lobortis velit vel odio hendrerit consectetur. Fusce ac nunc vel enim tincidunt vestibulum ut eu purus. Proin placerat nulla mi, a accumsan orci blandit vitae. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam consectetur justo dolor, ac sollicitudin neque fermentum ac.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Scripts requis pour Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
 <?php 
 
 var_dump($_POST['content']);
